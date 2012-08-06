d4d67212f84498d02c0b3c754eced4649974c6b6
Girvan Liu <girvanliu@gmail.com>
2012-08-06 12:14:38 +0800

    add codes

---
+++ javascript/mobiledetectioin.js
@@ -0,0 +1,29 @@
+// this javascript will detect mobile/tablet device and provide a proper string on window._device and html's className
+// You can write custom css for those device and access window._device via javascript
+// iphone / ipod touch: touch ios mobile iphone
+// ipad: touch ios tablet ipad
+// android phone: touch tablet android
+// please compress following code at http://refresh-sf.com/yui/
+(function(){
+
+ var device = '', _ipad = 'ipad', _iphone = 'iphone', ua = navigator.userAgent.toLowerCase();
+ if( ua.indexOf(_ipad) != -1 )
+     device = 'ios tablet ' + _ipad;
+ else if( ua.indexOf(_iphone) != -1 )
+     device = 'ios mobile ' + _iphone;
+ else if( ua.match(/android/) )
+ {
+     device = 'android';
+     if(ua.indexOf('mobile')==-1)
+         device += ' tablet';
+     else
+         device += ' mobile';
+ }
+ if(device)
+ {
+     device = 'touch ' + device;
+     document.documentElement.className += ' ' + device;
+ }
+ window._device = device;
+}
+)();
